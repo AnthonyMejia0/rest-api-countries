fc52dbf1d33b6fb88bc0056d6a5125d08e904740
AnthonyMejia0 <mejia.anthony96@gmail.com>
2022-08-21 21:30:07 -0700

    set height and colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useRecoilValue } from "recoil";
+import { darkAtomState } from "./atoms/darkAtom";
+import Countries from "./components/Countries";
+import Header from "./components/Header";
 
 function App() {
+  const darkMode = useRecoilValue(darkAtomState);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className={`h-screen overflow-x-hidden ${
+        darkMode
+          ? "bg-very-dark-Blue text-white"
+          : "bg-very-light-Gray text-very-dark-Blue"
+      }`}
+    >
+      <Header />
+      <Countries />
     </div>
   );
 }
