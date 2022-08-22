28ab471d5ace4506f9c5f4e5ff7acc837a0eaddb
AnthonyMejia0 <mejia.anthony96@gmail.com>
2022-08-22 03:23:06 -0700

    added react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,47 @@
+import { Route, Routes } from "react-router-dom";
 import { useRecoilValue } from "recoil";
 import { darkAtomState } from "./atoms/darkAtom";
 import Countries from "./components/Countries";
+import CountryDetails from "./components/CountryDetails";
 import Header from "./components/Header";
 
 function App() {
   const darkMode = useRecoilValue(darkAtomState);
 
   return (
-    <div
-      className={`h-screen overflow-x-hidden ${
-        darkMode
-          ? "bg-very-dark-Blue text-white"
-          : "bg-very-light-Gray text-very-dark-Blue"
-      }`}
-    >
-      <Header />
-      <Countries />
-    </div>
+    <Routes>
+      <Route
+        path="/details"
+        element={
+          <div
+            className={`h-screen overflow-x-hidden ${
+              darkMode
+                ? "bg-very-dark-Blue text-white"
+                : "bg-very-light-Gray text-very-dark-Blue"
+            }`}
+          >
+            <Header />
+            <CountryDetails />
+          </div>
+        }
+      />
+
+      <Route
+        path="/"
+        element={
+          <div
+            className={`h-screen overflow-x-hidden ${
+              darkMode
+                ? "bg-very-dark-Blue text-white"
+                : "bg-very-light-Gray text-very-dark-Blue"
+            }`}
+          >
+            <Header />
+            <Countries />
+          </div>
+        }
+      />
+    </Routes>
   );
 }
 
