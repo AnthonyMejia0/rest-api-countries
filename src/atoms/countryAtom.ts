2a945bfa80127477d12e836eb5e8f5d6c0516f81
AnthonyMejia0 <mejia.anthony96@gmail.com>
2022-08-24 00:35:58 -0700

    made atoms persistent

--- src/atoms/countryAtom.ts
+++ src/atoms/countryAtom.ts
@@ -1,7 +1,11 @@
 import { atom } from "recoil";
+import { recoilPersist } from "recoil-persist";
 import { countryType } from "../types/types";
 
+const { persistAtom } = recoilPersist();
+
 export const countryAtomState = atom({
-    key: "countryAtomState",
-    default: {} as countryType
+  key: "countryAtomState",
+  default: {} as countryType,
+  effects_UNSTABLE: [persistAtom],
 });
--- src/atoms/darkAtom.ts
+++ src/atoms/darkAtom.ts
@@ -1,6 +1,10 @@
 import { atom } from "recoil";
+import { recoilPersist } from "recoil-persist";
+
+const { persistAtom } = recoilPersist();
 
 export const darkAtomState = atom({
-    key: "darkAtomState",
-    default: false,
+  key: "darkAtomState",
+  default: false,
+  effects_UNSTABLE: [persistAtom],
 });
