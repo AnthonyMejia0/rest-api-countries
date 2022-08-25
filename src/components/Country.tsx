ee8cc3fc964b97aaf792ab90784ec5ea18542ecd
AnthonyMejia0 <mejia.anthony96@gmail.com>
2022-08-25 01:43:36 -0700

    added article tag

--- src/components/Country.tsx
+++ src/components/Country.tsx
@@ -10,7 +10,7 @@ function Country({ country }: { country: countryType }) {
 
   return (
     <Link to={"/details"} onClick={() => setcountry(country)}>
-      <div
+      <article
         className={`w-[18rem] md:w-[13rem] 3xl:w-[16.5rem] overflow-hidden rounded-md shadow-lg cursor-pointer ${
           darkMode ? "bg-dark-blue" : "bg-white"
         }`}
@@ -18,7 +18,7 @@ function Country({ country }: { country: countryType }) {
         <img
           className="w-full h-[10rem] md:h-[7.5rem] 3xl:h-[9rem] object-cover"
           src={country.flags.svg}
-          alt="Country Flag"
+          alt={`${country.name.common} Flag`}
         />
         <div className="flex flex-col items-start space-y-4 md:space-y-2 pt-7 px-7 md:pt-4 md:pl-4 pb-10">
           <h2 className="font-extrabold text-xl md:text-base text-center">
@@ -37,7 +37,7 @@ function Country({ country }: { country: countryType }) {
             </p>
           </div>
         </div>
-      </div>
+      </article>
     </Link>
   );
 }
